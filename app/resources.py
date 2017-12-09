453c86918d4d0ca238aff1c5025cd7479d458b71
Piotr Roksela <piotr@roksela.pl>
2017-12-09 19:41:50 +0100

    resources get

--- app/resources.py
+++ app/resources.py
@@ -1,14 +1,29 @@
-from flask_restful import Resource
+from flask_restful import Resource, fields, marshal_with
 
+from app.models import Event
 
-# TODO: all input/output fields in camelCase
+event_fields = {
+    'id': fields.Integer,           # read only
+    'name': fields.String,          # required
+    'description': fields.String,
+    'startTime': fields.DateTime(
+        dt_format='iso8601', attribute='start_time'),  # required
+    'endTime': fields.DateTime(
+        dt_format='iso8601', attribute='end_time'),
+    'total': fields.Integer,        # required
+    'taken': fields.Integer,        # read only
+    'available': fields.Integer,    # read only
+}
+
 
 class EventListResource(Resource):
+
+    @marshal_with(event_fields)
     def get(self):
         """
         Returns all events list
         """
-        return {'message': 'hello'}
+        return Event.query.all()
 
     def post(self):
         """
@@ -18,12 +33,14 @@ class EventListResource(Resource):
 
 
 class EventResource(Resource):
+
+    @marshal_with(event_fields)
     def get(self, event_id):
         """
         Returns specific event data
         :param event_id:
         """
-        return {'message': event_id}
+        return Event.query.get(event_id)
 
     def post(self, event_id):
         """
--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,24 +1,58 @@
 import json
-from datetime import datetime, timedelta
+from datetime import datetime
 from app.models import Event
 from tests.utils import TestCase
 
 
-class EventResourceTest(TestCase):
+class EventResourcesTest(TestCase):
 
     def setUp(self):
-        super(EventResourceTest, self).setUp()
+        super(EventResourcesTest, self).setUp()
         with self.app.app_context():
             self.event = Event(
-                name="Mum's Birthday", description="Great party!",
-                start_time=datetime.now() + timedelta(days=5),
-                end_time=datetime.now() + timedelta(days=5, hours=3),
+                name="Mum's Birthday",
+                description="Great party",
+                start_time=datetime(year=2018, month=4, day=7, hour=15),
+                end_time=datetime(year=2018, month=4, day=7, hour=18),
                 total=10, taken=0,
             )
             self.db.session.add(self.event)
+            another_event = Event(
+                name="Dad's Birthday",
+                description="Even better party",
+                start_time=datetime(year=2018, month=10, day=4, hour=15),
+                end_time=datetime(year=2018, month=10, day=4, hour=21),
+                total=10, taken=0,
+            )
+            self.db.session.add(another_event)
             self.db.session.commit()
+            self.event_id = self.event.id
 
-    def test_events(self):
+    def test_event_list_get(self):
         resp = self.app_client.get('/events')
         data = json.loads(resp.get_data(as_text=True))
-        self.assertDictEqual({'message': 'hello'}, data)
+        self.assertEqual(len(data), 2)
+        self.assertDictEqual(data[0], {
+            'id': self.event_id,
+            'name': "Mum's Birthday",
+            'description': "Great party",
+            'startTime': '2018-04-07T15:00:00',
+            'endTime': '2018-04-07T18:00:00',
+            'total': 10,
+            'taken': 0,
+            'available': 10,
+        })
+
+    def test_event_get(self):
+        resp = self.app_client.get('/events/%s' % self.event_id)
+        data = json.loads(resp.get_data(as_text=True))
+        self.assertDictEqual(data, {
+            'id': self.event_id,
+            'name': "Mum's Birthday",
+            'description': "Great party",
+            'startTime': '2018-04-07T15:00:00',
+            'endTime': '2018-04-07T18:00:00',
+            'total': 10,
+            'taken': 0,
+            'available': 10,
+        })
