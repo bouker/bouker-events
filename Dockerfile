f64e50e23a5ec622215e91dfb159105cde74b122
Piotr Roksela <piotr@roksela.pl>
2017-12-09 20:48:46 +0100

    correct configuration of docker and db

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,9 @@ WORKDIR /bouker
 ADD . /bouker/
 RUN pip install -r requirements.txt
 
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod 777 /docker-entrypoint.sh
+
 EXPOSE 8000
-ENTRYPOINT ["/usr/local/bin/gunicorn", "--config", "server.ini", "app.wsgi:app"]
+ENTRYPOINT ["/docker-entrypoint.sh"]
+CMD ["init"]
--- app/config.py
+++ app/config.py
@@ -1,10 +1,4 @@
 import os
 
-db_name = os.getenv("DB_NAME")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@localhost/{}'.format(
-    db_user, db_password, db_name,
-)
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite://")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
