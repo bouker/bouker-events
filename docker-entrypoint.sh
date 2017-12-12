505845ec21df699e45b205efd3fd8b303a1ddda9
Piotr Roksela <piotr@roksela.pl>
2017-12-12 17:19:14 +0100

    entrypoint echo update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,7 @@ CMD=$1
 
 case "$CMD" in
   "init" )
-    echo $SQLALCHEMY_DATABASE_URI
-    echo $FLASK_APP
+    echo "Starting Bouker-Events service"
     sleep 10
     flask db upgrade
     /usr/local/bin/gunicorn --config server.ini app.wsgi:app
