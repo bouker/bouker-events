96d9929b4d0b12791cc26345dd1e8a368d66a32b
Piotr Roksela <piotr@roksela.pl>
2018-01-26 01:07:27 +0100

    fixed tests

--- app/logging.py
+++ app/logging.py
@@ -2,5 +2,14 @@ import os
 from pygelf import GelfHttpHandler
 
 
-host = os.environ.get('BOUKER_LOGGING_URL')
-default_handler = GelfHttpHandler(host=host, port=12201, _facility='bouker-events')
+class GraylogHandler(GelfHttpHandler):
+
+    def emit(self, record):
+        try:
+            super(GraylogHandler, self).emit(record)
+        except Exception as e:
+            print("Cannot access remote Graylog Service: %s" % e)
+
+
+host = os.environ.get('BOUKER_LOGGING_URL', '')
+default_handler = GraylogHandler(host=host, port=12201, _facility='bouker-events')
